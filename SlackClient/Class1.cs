94498e0cb3ea0da69016b9a971e1305f7c6c8d29
Seth Coussens <seth.coussens@gmail.com>
2017-06-18 12:45:31 -0500

    Add project files.

---
+++ SlackClient/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackClient
+{
+    public class Class1
+    {
+    }
+}
