9918f90eaf7295e3309f76129c500b612136f958
Seth Coussens <seth.coussens@gmail.com>
2017-07-18 21:38:10 -0500

    v1.0.10.0
    added group functions

--- SlackWebApiClient/Interfaces/Methods/IGroups.cs
+++ SlackWebApiClient/Interfaces/Methods/IGroups.cs
@@ -6,7 +6,9 @@ namespace SlackWebApiClient.Interfaces.Methods
     public interface IGroups
     {
         Task<GroupResponse> Info(string group);
-        Task<GroupsResponse> List();
+        Task<GroupsResponse> List(bool excludeArchive, bool excludeMembers);
         Task<MessagesResponse> Messages(string channel, string ts = null, string latest = null, string oldest = null, int? count = default(int?), bool inclusive = false, bool unreads = false);
+        Task<GroupResponse> Invite(string channel, string user);
+        Task<Response> Kick(string channel, string user);
     }
 }
--- SlackWebApiClient/Methods/Groups.cs
+++ SlackWebApiClient/Methods/Groups.cs
@@ -14,7 +14,7 @@ namespace SlackWebApiClient.Methods
             _client = client;
         }
 
-        public async Task<GroupsResponse> List()
+        public async Task<GroupsResponse> List(bool excludeArchive = true, bool excludeMembers = false)
         {
             var endpoint = $"groups.list";
 
@@ -46,6 +46,32 @@ namespace SlackWebApiClient.Methods
 
             return await _client.GetMessages<MessagesResponse>(endpoint, channel, ts, latest, oldest, count, inclusive,
                 unreads);
+        }
+
+        public async Task<GroupResponse> Invite(string channel, string user)
+        {
+            var endpoint = $"groups.invite";
+
+            var body = new Dictionary<string, string>
+            {
+                {"channel", channel},
+                {"user", user }
+            };
+
+            return await _client.Post<GroupResponse>(endpoint, body);
+        }
+
+        public async Task<Response> Kick(string channel, string user)
+        {
+            var endpoint = $"groups.kick";
+
+            var body = new Dictionary<string, string>
+            {
+                {"channel", channel},
+                {"user", user }
+            };
+
+            return await _client.Post<GroupResponse>(endpoint, body);
         }
     }
 }
